e6b13e3b565df5f2e8eb909cc5fb98826f8db5db
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-12 15:44:58 -0300

    First component

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import NotificationButton from './components/NotificationButton'
+
 
 function App() {
   return (
-     <h1>Hello world</h1>
+    <div>
+      <h1>Hello world</h1>
+      <NotificationButton />
+    </div>
+     
   )
 }
 
